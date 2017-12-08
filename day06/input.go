00c20a8be6bbcaa8b0c59fd3933996889ba65138
Ben Holland <fivegreenapples@users.noreply.github.com>
2017-12-08 00:14:41 +0000

    Day 6

--- day06/input.go
+++ day06/input.go
@@ -1,3 +1,4 @@
 package main
 
-// var input = 
+var testInput = []int{0, 2, 7, 0}
+var input = []int{4, 1, 15, 12, 0, 9, 9, 5, 5, 8, 7, 3, 14, 5, 12, 3}
--- day06/main.go
+++ day06/main.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	fmt.Println("day 06")
+	loopsTest, sizeTest := calc(testInput)
+	fmt.Println("Test1:", loopsTest, sizeTest)
+	loops, size := calc(input)
+	fmt.Println("Answer:", loops, size)
+}
+
+func calc(input []int) (int, int) {
+
+	memory := make([]int, len(input))
+	copy(memory, input)
+	memorySize := len(memory)
+	knownArrangements := map[string]int{}
+	loops := 0
+
+	for {
+		currentArrangement := joinInts(memory, ",")
+		if loopNum, found := knownArrangements[currentArrangement]; found {
+			return loops, loops - loopNum
+		}
+		knownArrangements[currentArrangement] = loops
+		loops++
+
+		largestBank := 0
+		for bank, blocks := range memory {
+			if blocks > memory[largestBank] {
+				largestBank = bank
+			}
+		}
+
+		// Get number of blocks to redistribute
+		redistBlocks := memory[largestBank]
+		// zero the bank
+		memory[largestBank] = 0
+		// distribute the blocks over the memory
+		for j := 1; j <= redistBlocks; j++ {
+			memory[(largestBank+j)%memorySize]++
+		}
+	}
+
+}
+
+func joinInts(in []int, sep string) string {
+	intermediate := make([]string, len(in))
+	for i, v := range in {
+		intermediate[i] = strconv.Itoa(v)
+	}
+	return strings.Join(intermediate, sep)
 }
